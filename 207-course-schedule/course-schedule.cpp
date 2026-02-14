bcf0fc5d31d4cfd655bf0b4a9f3a954b33c6585a
Denish099 <gyldenish@gmail.com>
2026-02-14 14:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (81.72%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> list(numCourses);
+        vector<int> inDegree(numCourses, 0);
+
+        for (auto& edge : prerequisites) {
+            int u = edge[1];
+            int v = edge[0];
+            list[u].push_back(v);
+            inDegree[v]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            ans.push_back(curr);
+
+            for (auto& x : list[curr]) {
+                inDegree[x]--;
+                if (inDegree[x] == 0) {
+                    q.push(x);
+                }
+            }
+        }
+
+        return ans.size() == numCourses;
+    }
+};
