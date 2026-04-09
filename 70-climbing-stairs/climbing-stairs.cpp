d5f0b081489d27c41913dd67e4d38e065ff705d6
Denish099 <gyldenish@gmail.com>
2026-04-09 14:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.15%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dp;
+    int helper(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = helper(n-1) + helper(n-2);
+    }
+    int climbStairs(int n) {
+        dp.resize(n+1,-1);
+        return helper(n);
+    }
+};
