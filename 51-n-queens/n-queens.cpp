381890251ecc66b3757d9671de4a78f619355cad
Denish099 <gyldenish@gmail.com>
2026-04-12 13:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (31.69%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -3,46 +3,40 @@ public:
     vector<vector<string>> ans;
     vector<string> board;
 
-    bool isSafe(int row, int col, int n) {
+    bool check(int n, int row, int col) {
         for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
+            if (board[i][col] == 'Q') return false;
         }
 
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
+            if (board[i][j] == 'Q') return false;
         }
 
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
+            if (board[i][j] == 'Q') return false;
         }
 
         return true;
     }
 
-    void solve(int row, int n) {
+    void helper(int n, int row) {
         if (row == n) {
             ans.push_back(board);
             return;
         }
 
         for (int col = 0; col < n; col++) {
-            if (isSafe(row, col, n)) {
+            if (check(n, row, col)) {
                 board[row][col] = 'Q';
-                solve(row + 1, n);
+                helper(n, row + 1);
                 board[row][col] = '.';
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        board = vector<string>(n, string(n, '.'));
-        solve(0, n);
+        board.resize(n, string(n, '.'));
+        helper(n, 0);
         return ans;
     }
 };
