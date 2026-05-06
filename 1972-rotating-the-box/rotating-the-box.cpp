c0c19989552765dc038372d53b0fe7fc71c7ff45
Denish099 <gyldenish@gmail.com>
2026-05-06 15:27:31 +0530

    Time: 11 ms (65.85%) | Memory: 56.9 MB (15.72%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            int emptySpot = n - 1; 
+            
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    
+                    emptySpot = j - 1; 
+                } else if (boxGrid[i][j] == '#') {
+                   
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][emptySpot] = '#';
+                    
+                    emptySpot--; 
+                }
+            }
+        }
+
+        vector<vector<char>> ans(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = boxGrid[i][j]; 
+            }
+        }
+
+        return ans;
+    }
+};
