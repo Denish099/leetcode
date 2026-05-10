6db33ae66b0397a3185b8003922d780f9f3041d8
Denish099 <gyldenish@gmail.com>
2026-05-10 11:24:58 +0530

    Time: 16 ms (72.67%) | Memory: 68.7 MB (19.88%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -5,14 +5,12 @@ public:
     int helper(vector<int>& nums, int target, int i) {
         if (i == nums.size() - 1) return 0;
         
-        // Check against -2 now!
         if (dp[i] != -2) {
             return dp[i];
         }
 
         int max_jumps = -1; 
         for (int j = i + 1; j < nums.size(); j++) {
-            // Using long long to safely prevent integer overflow on massive numbers
             if (abs((long long)nums[j] - nums[i]) <= target) {
                 int jumps_from_j = helper(nums, target, j);
                 if (jumps_from_j != -1) {
@@ -27,7 +25,6 @@ public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
         
-        // Initialize with -2 (uncalculated)
         dp.assign(n, -2); 
         
         return helper(nums, target, 0);
