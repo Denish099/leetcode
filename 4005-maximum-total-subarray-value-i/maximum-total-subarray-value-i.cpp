6ae783af851a8f2cd573483b1e82d02b1d5ee17b
Denish099 <gyldenish@gmail.com>
2026-06-09 11:09:39 +0530

    Time: 4 ms (33.07%) | Memory: 103.7 MB (69.26%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        auto [min_it, max_it] = std::minmax_element(nums.begin(), nums.end());
+        
+       
+        long long max_diff = static_cast<long long>(*max_it) - *min_it;
+        
+        return max_diff * k;
+    }
+};
