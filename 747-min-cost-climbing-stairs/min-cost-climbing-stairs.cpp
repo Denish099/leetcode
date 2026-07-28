181766bc159c3ed126642448edaa3b500602ea7e
Denish099 <gyldenish@gmail.com>
2026-07-28 15:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.75%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    vector<int> dp;
-    
-    int helper(int idx, vector<int>& cost) {
-        // Base case: if we reach or pass the top floor, no more cost is needed.
-        if (idx >= cost.size()) {
-            return 0;
-        }
-        
-        // Return the already computed result to avoid recomputing overlapping subproblems.
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        
-        // Pay the cost at the current step, then take the minimum of a 1-step or 2-step jump.
-        return dp[idx] = cost[idx] + min(helper(idx + 1, cost), helper(idx + 2, cost));
-    }
-    
+    // int helper(int idx, vector<int>& cost) {
+    //     if (idx >= cost.size()) {
+    //         return 0;
+    //     }
+
+    //     if (dp[idx] != -1) {
+    //         return dp[idx];
+    //     }
+
+    //     return dp[idx] = cost[idx] + min(helper(idx + 1, cost), helper(idx +
+    //     2, cost));
+    // }
+
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        dp.resize(n, -1); // We only need size 'n' because valid indices are 0 to n-1
+        if(n==1){
+            return 0;
+        }
+        vector<int> dp(n+2, -1);
+
+        dp[n] = 0;
+        dp[n+1] = 0;
+
+        for(int i = n-1;i>=0;i--){
+            dp[i] = cost[i] + min(dp[i+1],dp[i+2]);
+        }
+
+        return dp[0] > dp[1] ? dp[1] : dp[0];
         
-        // We can start from either index 0 or index 1
-        return min(helper(0, cost), helper(1, cost));
     }
 };
