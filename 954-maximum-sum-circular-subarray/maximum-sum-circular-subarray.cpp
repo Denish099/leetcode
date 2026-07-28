dc42e3a0bcadbe3b19c46be6cab8a3a84d104f3f
Denish099 <gyldenish@gmail.com>
2026-07-28 22:56:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (49.19%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& A) {
-        int total_sum = 0, curr_sum1 = 0, curr_sum2 = 0, mxsum_subary = INT_MIN,
-            minsum_subary = INT_MAX;
-        for (auto i : A) {
-            total_sum += i;
-            curr_sum1 += i;
-            curr_sum2 += i;
-            mxsum_subary = max(mxsum_subary, curr_sum1);
-            if (curr_sum1 < 0)
-                curr_sum1 = 0;
-            minsum_subary = min(curr_sum2, minsum_subary);
-            if (curr_sum2 > 0)
-                curr_sum2 = 0;
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int totalSum = 0;
+        int currMinSum = 0;
+        int currMaxSum = 0;
+        int maxSum = INT_MIN;
+        int minSum = INT_MAX;
+
+        bool flag = false;
+
+        for (auto num : nums) {
+            if(num > 0){
+                flag = true;
+            }
+            totalSum += num;
+            currMinSum += num;
+            currMaxSum += num;
+
+            maxSum = max(maxSum, currMaxSum);
+            minSum = min(minSum, currMinSum);
+
+            if (currMinSum > 0) {
+                currMinSum = 0;
+            }
+            if (currMaxSum < 0) {
+                currMaxSum = 0;
+            }
         }
-        return (total_sum == minsum_subary)
-                   ? mxsum_subary
-                   : max(mxsum_subary, total_sum - minsum_subary);
+
+        if(totalSum < 0 && !flag){
+            return maxSum;
+        }
+
+        return max(maxSum,totalSum - minSum);
     }
 };
