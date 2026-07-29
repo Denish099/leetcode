a1864f6aa26f8398988eeec126e8d63d7013e7d0
Denish099 <gyldenish@gmail.com>
2026-07-29 23:06:42 +0530

    Time: 377 ms (25.00%) | Memory: 203.3 MB (5.05%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int n;
     vector<vector<int>> dp;
+    int helper(vector<int> nums, int target, int idx, int sum) {
 
-    int helper(vector<int>& nums, int idx, int sum, int target) {
-        if (idx == n)
-            return sum == target;
+        if (sum == target && idx >= nums.size()) {
+            return 1;
+        }
 
-        if (dp[idx][sum + 1000] != -1)
+        if (idx >= nums.size()) {
+            return 0;
+        }
+        if (dp[idx][sum + 1000] != -1) {
             return dp[idx][sum + 1000];
-
-        int add = helper(nums, idx + 1, sum + nums[idx], target);
-        int sub = helper(nums, idx + 1, sum - nums[idx], target);
+        }
+        int add = helper(nums, target, idx + 1, sum + nums[idx]);
+        int sub = helper(nums, target, idx + 1, sum - nums[idx]);
 
         return dp[idx][sum + 1000] = add + sub;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        n = nums.size();
+        dp.resize(nums.size(), vector<int>(2001, -1));
 
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if (abs(target) > total)
-            return 0;
-
-        dp.assign(n, vector<int>(2001, -1));
-
-        return helper(nums, 0, 0, target);
+        return helper(nums, target, 0, 0);
     }
 };
