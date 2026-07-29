dc48d09b8735a42b7f54889c352d6195063d3a4e
Denish099 <gyldenish@gmail.com>
2026-07-29 10:33:02 +0530

    Time: 179 ms (21.45%) | Memory: 13.7 MB (43.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
+    int t[1001][1001];
+    bool solve(string& s, int i, int j) {
+        if (i >= j) {
+            return 1;
+        }
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
+
+        if (s[i] == s[j]) {
+            return t[i][j] = solve(s, i + 1, j - 1);
+        }
+
+        return t[i][j] = 0;
+    }
     string longestPalindrome(string s) {
+        memset(t, -1, sizeof(t));
         int n = s.size();
+        int maxLen = INT_MIN;
+        int sp = 0;
 
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for (int i = 0; i < n; i++) {
 
-        int start = 0;
-        int len = 1;
-
-        
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        for (int l = 2; l <= n; l++) {
-
-            for (int i = 0; i + l - 1 < n; i++) {
-
-                int j = i + l - 1;
-
-                if (s[i] != s[j])
-                    dp[i][j] = false;
-
-                else {
-
-                    if (l == 2)
-                        dp[i][j] = true;
-
-                    else
-                        dp[i][j] = dp[i + 1][j - 1];
-
-                    if (dp[i][j] && l > len) {
-                        len = l;
-                        start = i;
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        sp = i;
                     }
                 }
             }
         }
 
-        return s.substr(start, len);
+        return s.substr(sp, maxLen);
     }
 };
