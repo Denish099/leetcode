909fc56c695700347cbc02df1311d5e573e84e4b
Denish099 <gyldenish@gmail.com>
2026-07-30 12:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.81%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans = 0;
+        for (int i = 0; i < word.size(); i++) {
+            if (i < 8) {
+                ans += 1;
+            } else if (i <= 15) {
+                ans += 2;
+            } else if (i <= 23) {
+                ans += 3;
+
+            } else {
+                ans += 4;
+            }
+        }
+
+        return ans;
+    }
+};
