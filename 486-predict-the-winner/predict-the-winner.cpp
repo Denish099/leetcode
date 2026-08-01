38a191c6ee57dc819a847df00e0f55046871886f
Denish099 <gyldenish@gmail.com>
2026-08-01 11:05:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+
     int solve(vector<int>& nums, int i, int j) {
 
         if(i > j){
             return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
         int take_i =
@@ -13,11 +19,12 @@ public:
             nums[j] + min(solve(nums, i, j - 2), solve(nums, i + 1, j - 1));
 
 
-        return max(take_i,take_j);
+        return dp[i][j] = max(take_i,take_j);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        dp.resize(n+1,vector<int>(n+1,-1));
 
         int totalScore = accumulate(nums.begin(), nums.end(), 0);
 
