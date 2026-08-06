08c3e69197cf9cb469d02bb0201a8f371d49b10b
Denish099 <gyldenish@gmail.com>
2026-08-06 14:03:16 +0530

    Time: 8 ms (18.69%) | Memory: 61.8 MB (36.93%) - LeetSync

--- 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.cpp
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     int getMaxLen(vector<int>& nums) {
         int n = nums.size();
-
-        int left = 0;
-        int negPos = -1;
-        int countNeg = 0;
-
+        int start = 0;
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
+        int countNeg = 0;
+        int negPos = -1;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 0) {
-               
-                left = i + 1;
+                start = i + 1;
                 negPos = -1;
                 countNeg = 0;
                 continue;
             }
+
             if (nums[i] < 0) {
                 countNeg++;
-                if(negPos == -1){
+                if (negPos == -1) {
                     negPos = i;
                 }
             }
 
-            if (countNeg % 2 == 0){
-                ans = max(ans,i - left + 1);
-            }else{
-                ans = max(ans,i - negPos);
-
+            if (countNeg % 2 == 0) {
+                ans = max(ans, i - start + 1);
+            } else {
+                ans = max(ans, i - negPos);
             }
         }
 
