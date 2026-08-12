7c88d7a502d64ad6de9b33fd3ceddef1c1f2c6b4
Denish099 <gyldenish@gmail.com>
2026-08-12 15:05:05 +0530

    Time: 8 ms (5.10%) | Memory: 42.1 MB (34.43%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+        vector<int> suffix(n, 0);
+
         int mul = 1;
-        int n= nums.size();
-        vector<int> prefix;
-        for(int i = 0;i<n;i++){
-            prefix.push_back(mul);
-            mul*=nums[i];
-        }
-        mul=1;
-
-        vector<int> suffix;
-
-        for(int i = n-1;i>=0;i--){
-            suffix.push_back(mul);
-            mul*=nums[i];
+        for (int i = 0; i < n; i++) {
+            mul *= nums[i];
+            prefix[i] = mul;
         }
 
-        reverse(suffix.begin(),suffix.end());
+        mul = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            mul *= nums[i];
+            suffix[i] = mul;
+        }
 
-        vector<int> ans;
-
-        for(int i = 0;i<n;i++){
-            ans.push_back(suffix[i] * prefix[i]);
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && i < n - 1) {
+                ans[i] = prefix[i - 1] * suffix[i + 1];
+            } else if (i < 1) {
+                ans[i] = suffix[i + 1];
+            } else {
+                ans[i] = prefix[i - 1];
+            }
         }
 
         return ans;
-
     }
 };
