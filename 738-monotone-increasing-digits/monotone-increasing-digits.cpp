a8131e502fdcb2ed54749c7ca82a0119923bb1ba
Denish099 <gyldenish@gmail.com>
2026-08-14 17:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.14%) - LeetSync

--- 738-monotone-increasing-digits/monotone-increasing-digits.cpp
+++ 738-monotone-increasing-digits/monotone-increasing-digits.cpp
@@ -3,16 +3,31 @@ public:
     int monotoneIncreasingDigits(int n) {
         string s = to_string(n);
 
-        int idx = s.size();
+        int idx = -1;
 
-        for (int i = s.size() - 1; i > 0; i--) {
-            if (s[i - 1] > s[i]) {
-                s[i - 1]--;
+        // Find the last violation
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] > s[i + 1]) {
                 idx = i;
+                break;
             }
         }
 
-        for (int i = idx; i < s.size(); i++) {
+        if (idx == -1) {
+            return n;
+        }
+
+        // Decrease the problematic digit
+        s[idx]--;
+
+        // Propagate the correction to the left
+        while (idx > 0 && s[idx - 1] > s[idx]) {
+            s[idx - 1]--;
+            idx--;
+        }
+
+        // Everything after idx should be 9
+        for (int i = idx + 1; i < s.size(); i++) {
             s[i] = '9';
         }
 
