3bec7273baf447ae9d5a2f5055a760bb14ced2cb
Denish099 <gyldenish@gmail.com>
2026-08-20 12:44:10 +0530

    Time: 78 ms (83.11%) | Memory: 13.4 MB (93.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    bool solve(int i, int target, vector<vector<int>>& dp, vector<int>& arr) {
-        if (target == 0)
-            return true;
-        if (i == 0)
-            return target == arr[0];
-        if (dp[i][target] != -1)
-            return dp[i][target];
-        int nottake = solve(i - 1, target, dp, arr);
-        int take = false;
-        if (arr[i] <= target) {
-            take = solve(i - 1, target - arr[i], dp, arr);
-        }
-
-        return dp[i][target] = take || nottake;
-    }
     bool canPartition(vector<int>& nums) {
 
-        int sum = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        if (sum % 2 != 0)
-            return false;
-        int target = sum / 2;
+        int total = accumulate(nums.begin(), nums.end(), 0);
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        return solve(n - 1, target, dp, nums);
+        if (total % 2 != 0)
+            return false;
+
+        int target = total / 2;
+
+        vector<bool> dp(target + 1, false);
+
+        dp[0] = true;
+
+        for (int num : nums) {
+
+            for (int s = target; s >= num; s--) {
+
+                dp[s] = dp[s] || dp[s - num];
+            }
+        }
+
+        return dp[target];
     }
 };
