6e940e75bf05a57b1857ebf84ff9109fae33ccad
Denish099 <gyldenish@gmail.com>
2026-08-27 12:28:18 +0530

    Time: 8 ms (36.94%) | Memory: 17.1 MB (22.93%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string result = "";
+    bool solve(string& curr,vector<int>&count,string target,int idx,bool greater){
+        if(idx == target.size()){
+            if(greater){
+                result = curr;
+                return true;
+            }
+            return false;
+        }
+
+        for(char ch ='a';ch <= 'z';ch++){
+            if(count[ch-'a'] == 0){
+                continue;
+            }
+
+            if(!greater && ch < target[idx]){
+                continue;
+            }
+
+            curr.push_back(ch);
+            count[ch-'a']--;
+
+            bool isGreater = greater || ch > target[idx];
+
+            if(solve(curr,count,target,idx+1,isGreater)){
+                return true;
+            }
+
+            curr.pop_back();
+            count[ch-'a']++;
+        }
+
+        return false;
+
+    }
+    
+    string lexGreaterPermutation(string s, string target) {
+        vector<int> count(26, 0);
+        for (auto& ch : s) {
+            count[ch - 'a']++;
+        }
+
+        string curr;
+
+        solve(curr, count, target, 0, false);
+
+        return result;
+    }
+};
