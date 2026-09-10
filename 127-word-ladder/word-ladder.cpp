3a3bfcb98038ef37e4592ab781ea79d92a9a7e42
Denish099 <gyldenish@gmail.com>
2026-09-11 00:03:58 +0530

    Time: 75 ms (41.05%) | Memory: 21.4 MB (30.51%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
+
         unordered_set<string> st(wordList.begin(), wordList.end());
+
+        if (!st.count(endWord))
+            return 0;
 
         int size = beginWord.size();
 
-        if (!st.count(endWord)) {
-            return 0;
-        }
-
-        set<string> vis;
+        unordered_set<string> vis;
         vis.insert(beginWord);
 
         queue<string> q;
@@ -19,37 +19,41 @@ public:
         int level = 0;
 
         while (!q.empty()) {
+
             int n = q.size();
 
             while (n--) {
+
                 string curr = q.front();
                 q.pop();
 
-                if (curr == endWord) {
+                if (curr == endWord)
                     return level + 1;
-                }
 
                 for (int i = 0; i < size; i++) {
 
                     char original = curr[i];
 
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == original) {
+
+                        if (c == original)
                             continue;
-                        }
 
                         curr[i] = c;
-                        if (!vis.count(curr) && st.count(curr)) {
-                            q.push(curr);
-                            vis.insert(curr);
-                        }
 
-                        curr[i] = original;
+                        if (st.count(curr) && !vis.count(curr)) {
+                            vis.insert(curr);
+                            q.push(curr);
+                        }
                     }
+
+                    curr[i] = original;
                 }
             }
+
             level++;
         }
+
         return 0;
     }
 };
