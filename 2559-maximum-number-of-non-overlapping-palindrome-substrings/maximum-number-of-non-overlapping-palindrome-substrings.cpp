f229a90c0ff58043e6fd99196fb378831b981c59
Denish099 <gyldenish@gmail.com>
2026-09-15 15:38:58 +0530

    Time: 235 ms (31.67%) | Memory: 19.4 MB (53.14%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-        int n = s.size(), ans = 0, end = -1;
-        for(int i = 0; i < n; i++){
-    
-            for(int l : {i - 1, i}){
-                int r = i;
-               
-                while(l >= 0 && r < n && s[l] == s[r]){
-                    if(r - l + 1 >= k && l > end){
-                        ans++;
-                        end = r;
-                        break;
-                    }
-                    l--, r++;
+
+        int n = s.size();
+
+
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+
+        for (int len = 1; len <= n; len++) {
+
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+
+                if (len == 1) {
+                    pal[i][j] = true;
+                }
+                else if (len == 2) {
+                    pal[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    pal[i][j] =
+                        (s[i] == s[j]) && pal[i + 1][j - 1];
                 }
             }
         }
-        return ans;
+
+
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+
+    
+            dp[i] = dp[i - 1];
+
+            for (int j = 0; j < i; j++) {
+
+                int len = i - j;
+
+                if (len >= k && pal[j][i - 1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
